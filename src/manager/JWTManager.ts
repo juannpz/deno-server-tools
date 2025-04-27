d5c4d84dfa8e51edf49799d58a73f39219524754
juannpz <juannpz@proton.me>
2025-04-27 14:25:59 -0300

    v0.9.0 - JWT Manager updated

--- src/manager/JWTManager.ts
+++ src/manager/JWTManager.ts
@@ -1,6 +1,14 @@
 import { create, type Header, verify, type Payload } from "https://deno.land/x/djwt@v3.0.2/mod.ts";
 import { buildResponse, type GenericResponse } from '../utils/http.ts';
 
+interface IGenerateKeyConfig {
+    format: Exclude<KeyFormat, "jwk">;
+    keyData: BufferSource;
+    algorithm: AlgorithmIdentifier | HmacImportParams | RsaHashedImportParams | EcKeyImportParams;
+    extractable: boolean;
+    keyUsages: KeyUsage[];
+}
+
 export class JWTManager {
     private static signature: string | null = null;
     private static textEncoder = new TextEncoder();
@@ -11,7 +19,7 @@ export class JWTManager {
         JWTManager.signature = signature;
     }
 
-    public static async verify<T extends Payload>(token: string): Promise<GenericResponse<T>> {
+    public static async verify<T extends Payload>(token: string, keyGenerationConfig: IGenerateKeyConfig): Promise<GenericResponse<T>> {
         if (!JWTManager.signature) {
             return buildResponse({
                 success: false,
@@ -20,7 +28,7 @@ export class JWTManager {
         }
 
         try {
-            const generateKeyResult = await JWTManager.generateKey();
+            const generateKeyResult = await JWTManager.generateKey(keyGenerationConfig);
 
             if (!generateKeyResult.success)
                 return generateKeyResult;
@@ -33,7 +41,7 @@ export class JWTManager {
         }
     }
 
-    public static async generate<T extends Payload>(configHeader: Header, payload: T): Promise<GenericResponse<string>> {
+    public static async generate<T extends Payload>(configHeader: Header, payload: T, keyGenerationConfig: IGenerateKeyConfig): Promise<GenericResponse<string>> {
         if (!JWTManager.signature) {
             return buildResponse({
                 success: false,
@@ -42,7 +50,7 @@ export class JWTManager {
         }
 
         try {
-            const generateKeyResult = await JWTManager.generateKey();
+            const generateKeyResult = await JWTManager.generateKey(keyGenerationConfig);
 
             if (!generateKeyResult.success)
                 return generateKeyResult;
@@ -55,16 +63,23 @@ export class JWTManager {
         }
     }
 
-    private static async generateKey(): Promise<GenericResponse<CryptoKey>> {
+    private static async generateKey(config: IGenerateKeyConfig): Promise<GenericResponse<CryptoKey>> {
+        if (!JWTManager.signature) {
+            return buildResponse({
+                success: false,
+                error: new Error('Missing JWT Manager signature. Call init() first')
+            });
+        }
+
         try {
-            const keyData = JWTManager.textEncoder.encode(JWTManager.signature!);
+            const keyData = JWTManager.textEncoder.encode(JWTManager.signature);
 
             const key = await crypto.subtle.importKey(
-                "raw",
+                config.format ?? "raw",
                 keyData,
-                { name: "HMAC", hash: "SHA-256" },
-                false,
-                ["sign", "verify"]
+                config.algorithm ?? { name: "HMAC", hash: "SHA-256" },
+                config.extractable ?? false,
+                config.keyUsages ?? ["sign", "verify"]
             );
 
             return buildResponse({ success: true, data: key });
